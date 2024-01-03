9cecc406d3c20d28ed30d22eec43d1dc0af5fe0b
Arshiya.tarannum <tarannum4@gmail.com>
2024-01-03 15:12:18 +0530

    first commit

--- Scripts.py
+++ Scripts.py
@@ -1,1 +1,43 @@
-# For All Task
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import xpath
+from xpath import *
+
+
+application_url = 'https://www.redbus.in/'
+
+def launching_redbus():
+    global driver
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument("--disable-notifications")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    driver.get(application_url)
+
+def selecting_cities():
+    driver.find_element(By.XPATH, xpath.From_source_field_xpath).send_keys('Mumbai')
+    time.sleep(2)
+    From_cities_suggestion_list = driver.find_elements(By.XPATH, xpath.from_cities_suggestion_xpath)
+    for city in From_cities_suggestion_list:
+        try :
+            if city.text == 'Borivali East':
+                city.click()
+                time.sleep(2)
+        except:
+            pass
+    driver.find_element(By.ID,"dest").send_keys('Bangalore')
+    time.sleep(2)
+    Destination_cities_suggestion_list = driver.find_elements(By.XPATH, xpath.destination_cities_suggestion_xpath)
+    for city in Destination_cities_suggestion_list:
+        try:
+            if city.text == 'Indiranagar':
+                city.click()
+                time.sleep(2)
+        except:
+            pass
+launching_redbus()
+selecting_cities()
--- xpath.py
+++ xpath.py
@@ -1,1 +1,4 @@
-
+From_source_field_xpath = '//input[@id="src"]'
+Destination_field_xpath = '//input[@id="dest"]'
+from_cities_suggestion_xpath = '//*[@class="sc-dnqmqq eFEVtU"]//li//text[1]'
+destination_cities_suggestion_xpath = '//ul[@class="sc-dnqmqq eFEVtU"]//li//text[1]'
