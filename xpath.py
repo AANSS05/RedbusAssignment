d8b0da7c6403ef80a11ada6295cb9d849215bb8e
Adityakumar.a <adityakumarlomte@gmail.com>
2024-01-03 21:47:20 +0530

    Xpath Added

--- xpath.py
+++ xpath.py
@@ -4,3 +4,8 @@ from_cities_suggestion_xpath = '//*[@class="sc-dnqmqq eFEVtU"]//li//text[1]'
 destination_cities_suggestion_xpath = '//ul[@class="sc-dnqmqq eFEVtU"]//li//text[1]'
 search_button_xpath = "//button[@id='search_button']"
 assertion_element_xpath = "//div[@class='title f-bold']"
+month_dates_xpath = '//*[contains(@class,"DayTilesWrapper")]//*[contains(@class,"isgDNj")]//child::span[not(contains(@class,"gigHYE"))]'
+next_month_dates_xpath = '//*[contains(@class,"DayTilesWrapper")]//*[contains(@class,"isgDNj")]//child::span[(contains(@class,"gigHYE"))]'
+
+
+
