a3ef730781ef9634745196469f621fcd6c00e338
AANSS05 <155517969+AANSS05@users.noreply.github.com>
2024-01-03 16:45:04 +0530

    Update xpath.py

--- xpath.py
+++ xpath.py
@@ -2,3 +2,5 @@ From_source_field_xpath = '//input[@id="src"]'
 Destination_field_xpath = '//input[@id="dest"]'
 from_cities_suggestion_xpath = '//*[@class="sc-dnqmqq eFEVtU"]//li//text[1]'
 destination_cities_suggestion_xpath = '//ul[@class="sc-dnqmqq eFEVtU"]//li//text[1]'
+search_button_xpath = "//button[@id='search_button']"
+assertion_element_xpath = "//div[@class='title f-bold']"
