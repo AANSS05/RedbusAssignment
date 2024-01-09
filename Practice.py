c57628a55c3a0e0301a5da0101b28fdbfca59a1b
Saurabh Badave <saurabhbadave@gmail.com>
2024-01-09 12:19:14 +0530

    My Filter Bus Function Added

--- Practice.py
+++ Practice.py
@@ -1,1 +1,5 @@
-
+def filter_buses():
+    driver.find_element(By.XPATH,xpath.first_filter_xpath).click()
+    time.sleep(3)
+    driver.find_element(By.XPATH,xpath.second_filter_xpath).click()
+    time.sleep(3)
