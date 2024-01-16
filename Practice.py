a83e81b0c042f02a62a8e3644dc2a4e7165c4109
Arshiya.tarannum <tarannum4@gmail.com>
2024-01-16 12:27:29 +0530

    my recent commit

--- Practice.py
+++ Practice.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import xpath
-from datetime import datetime,timedelta
+
 application_url = 'https://www.redbus.in/'
 
 def launching_redbus():
@@ -38,51 +38,3 @@ def selecting_cities():
         except:
             pass
 
-def scroll_up():
-    driver.execute_script("window.scrollTo(0,100)")
-
-def click_on_search_button():
-    driver.find_element(By.XPATH,xpath.search_button_xpath).click()
-    
-def filter_buses():
-    driver.find_element(By.XPATH,xpath.first_filter_xpath).click()
-    time.sleep(3)
-    driver.find_element(By.XPATH,xpath.second_filter_xpath).click()
-    time.sleep(3)
-
-def select_date():
-    month_dates = driver.find_elements(By.XPATH, xpath.month_dates_xpath)
-    next_dates = driver.find_elements(By.XPATH, xpath.next_month_dates_xpath)
-    import datetime
-    current_date = str(datetime.date.today().day)
-    current_month_dates = [date.text for date in month_dates]
-    next_month_dates = [date.text for date in next_dates]
-    try:
-        date_after_current_date = current_month_dates.index(current_date) + 2
-        month_dates[date_after_current_date].click()
-        time.sleep(2)
-    except:
-        next_dates[1].click()
-        time.sleep(2)
-    from datetime import datetime
-    date_after_2_days_from_current_date = str((datetime.now() + timedelta(days=2)).day)
-    date_after_selection= (driver.find_element(By.XPATH,xpath.selected_date_xpath).text).split(' ')[0]
-    assert date_after_selection == date_after_2_days_from_current_date
-
-def bus_with_low_fare():
-    driver.find_element(By.XPATH, xpath.Fare_link_xpath).click()
-    elements = driver.find_elements(By.XPATH, xpath.All_buses_details_xpath)
-    buses = [item.text for item in elements]
-    low_fare_bus = buses[0]
-    bus = [item for item in low_fare_bus.split('\n')]
-    for details in bus:
-        if details.startswith('INR'):
-            print(bus[0]+' has the lowest fair of '+details)
-
-launching_redbus()
-selecting_cities()
-select_date()
-scroll_up()
-click_on_search_button()
-filter_buses()
-bus_with_low_fare()
